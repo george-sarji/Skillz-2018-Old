f29e9a7e2f53f88a15ee1db2108070c292864863
unknown <sadaaboul1@gmail.com>
2018-02-03 12:56:35 +0200

    Added PushAsteroid Method

--- TryPush.cs
+++ TryPush.cs
@@ -7,6 +7,26 @@ namespace Bot
 {
     class TryPush : InitializationBot
     {
-        
+        var game = InitializationBot.game;
+        var EnemyPirates = game.GetEnemyLivingPirates();
+        var MyPirates = game.GetMyLivingPirates();
+        var Asteroids = game.GetLivingAsteroids();
+        public List<Pirate> PushAsteroid()
+        {
+            foreach (var Enemy in EnemyPirates)
+            {
+                foreach (var Piratre in MyPirates)
+                {
+                    foreach (var Asteroid in Asteroid)
+                    {
+                        var EnemyNextLocation = Enemy.Location.Towards(Asteroid.Location+Asteroid.Direction,Pirate.PushDistance);
+                        if(Pirate.CanPush(Enemy) && EnemyNextLocation.InRange(Asteroid.Location+Asteroid.Direction,Asteroid.Size))
+                        {
+                            Pirate.Push(Enemy,EnemyNextLocation);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
