6cf7e66f6765cedd20a92c2d6ffd521e468e3601
george17-meet <george17@meet.mit.edu>
2018-02-03 11:10:01 +0200

    Added capture capsules for the aggressive.

--- AggressiveBot.cs
+++ AggressiveBot.cs
@@ -13,6 +13,38 @@ namespace Bot
             foreach(var capsule in myCapsules)
             {
                 // Sort the pirates per the distance
+                myPirates = myPirates.OrderBy(pirate => pirate.Distance(capsule.InitialLocation)).ToList();
+                // Check if the capsule was taken.
+                if(capsule.Holder!=null && myPirates.Any())
+                {
+                    // Send the pirate to the capsule spawn
+                    var pirateSailer = myPirates.First();
+                    pirateSailer.Sail(capsule.InitialLocation);
+                    string message = "Pirate "+ pirateSailer.ToString() + " sails towards "+ capsule.InitialLocation;
+                    message.Print();
+                    myPirates = myPirates.Where(pirate => !pirate.Equals(pirateSailer)).ToList();
+                    // Get the pirate that has the capsule
+                    var capsuleHolder = capsule.Holder;
+                    // Send him to the closest city orderd by distance and value
+                    var bestMothership = myMotherships.OrderBy(mothership => mothership.Distance(capsuleHolder)).OrderBy(mothership => mothership.ValueMultiplier).FirstOrDefault();
+                    if(bestMothership!=null)
+                    {
+                        // Sail towards the city.
+                        capsuleHolder.Sail(bestMothership);
+                        message = "Pirate " + capsuleHolder.ToString() + " sails towards " + bestMothership.ToString();
+                        message.Print(); 
+                         myPirates = myPirates.Where(pirate => !pirate.Equals(capsuleHolder)).ToList();
+                    }
+                }
+                else if(myPirates.Any())
+                {
+                    // Send the closest pirate to capture the capsule.
+                    var sailingPirate = myPirates.First();
+                    sailingPirate.Sail(capsule.InitialLocation);
+                    string message = "Pirate "+ sailingPirate.ToString() + " sails towards "+ capsule.InitialLocation;
+                    message.Print();
+                     myPirates = myPirates.Where(pirate => !pirate.Equals(sailingPirate)).ToList();
+                }
             }
         }
     }
