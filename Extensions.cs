1f5c22faf4a33f3d3ae1a86cab285a21685b1418
george17-meet <george17@meet.mit.edu>
2018-02-12 09:04:07 +0200

    Added wormhole extension functions.

--- Extensions.cs
+++ Extensions.cs
@@ -107,6 +107,27 @@ namespace Bot
            return new Location((y1+y2)/2, (x1+x2)/2);
        }
 
+       private static int DistanceThroughWormhole(Location from, MapObject to, Wormhole wormhole, IEnumerable<Wormhole> wormholes)
+       {
+           return from.Distance(wormhole) +
+                  ClosestDistance(wormhole.Partner.Location, to,
+                                  wormholes.Where(w => w.Id != wormhole.Id && w.Id != wormhole.Partner.Id));
+       }
+
+       private static int ClosestDistance(Location from, MapObject to, IEnumerable<Wormhole> wormholes)
+       {
+           if (wormholes.Any())
+           {
+               int distanceWithoutWormholes = from.Distance(to);
+               int distanceWithWormholes = wormholes
+                   .Select(wormhole => DistanceThroughWormhole(from, to, wormhole, wormholes))
+                   .Min();
+               return System.Math.Min(distanceWithoutWormholes, distanceWithWormholes);
+           }
+           return from.Distance(to);
+       }
+
+
     }
 
 }
